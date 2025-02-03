17c7a54b9836f3ff66a30e07595e0915be6fdc8a
Kitzzu <nekoneko274nb@gmail.com>
2025-02-03 11:13:06 +0700

    added form view

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
-from . import custom_lead
-from . import models, my_custom_model, customer_rank
+from . import custom_leadfrom 
+import custom_lead
+from . import models, my_custom_model, custom_lead, customer_rank
 from . import custom_partner
 from . import employee
