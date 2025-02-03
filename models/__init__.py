f7421eda313991b206204a754fa86c536fb73039
Kitzzu <nekoneko274nb@gmail.com>
2025-02-03 14:47:37 +0700

    added additional info

--- models/__init__.py
+++ models/__init__.py
@@ -1,3 +1,4 @@
 # -*- coding: utf-8 -*-
 from . import custom_lead
 from . import models, my_custom_model
+from . import custom_partner
--- models/custom_lead.py
+++ models/custom_lead.py
@@ -4,3 +4,13 @@ class CustomLead(models.Model):
     _inherit = 'crm.lead'
 
     custom_field = fields.Char(string='Custom Field')
+    # x_partner_rank_id = fields.Many2one('res.partner.rank', string='Rank')
+    x_customer_type = fields.Selection(
+        [('nhap', 'Nháp'), ('don_vi_thu_3', 'Đơn vị thứ 3'), ('nha_dong_thung', 'Nhà đóng thùng')], 
+        string='Bên thứ 3/Nhà đóng thùng ', default='nhap'
+    )    
+    x_phone = fields.Char(string='Số điện thoại')
+    x_email_from = fields.Char(string='Email')
+    x_vat = fields.Many2one('res.partner', string='Số ĐKKD (Mã số thuế)') 
+    x_indentity_number = fields.Many2one('res.partner', string='CCCD/CMT')
+    x_industry_id = fields.Many2one('res.partner.industry', string='Lĩnh vực kinh doanh')
