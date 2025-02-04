5aad02d42a41d3b04724a84839605919ca87dc25
Vy <aum.vypt@gmail.com>
2025-02-04 13:41:56 +0700

    add brand field and view table

--- models/__init__.py
+++ models/__init__.py
@@ -2,5 +2,9 @@
 from . import custom_leadfrom 
 import custom_lead
 from . import models, my_custom_model, custom_lead, customer_rank
+<<<<<<< HEAD
 from . import custom_partner
 from . import employee
+=======
+from . import member_line, owned_team_car_line
+>>>>>>> 243ec33... add brand field and view table
--- models/custom_lead.py
+++ models/custom_lead.py
@@ -1,12 +1,21 @@
+<<<<<<< HEAD
+=======
+from odoo import models, fields, api
+>>>>>>> 243ec33... add brand field and view table
 
 import re
 
 from odoo import models, fields, api
 
+<<<<<<< HEAD
 class CustomLead(models.Model):
     _inherit = 'crm.lead'
+=======
+    # Notebook lines
+    x_member_line_ids = fields.One2many('member.line', 'lead_id', string='Member Lines')
+    x_owned_team_car_line_ids = fields.One2many('owned.team.car.line', 'lead_id', string='Owned Team Car Lines')
+>>>>>>> 243ec33... add brand field and view table
 
-    custom_field = fields.Char(string='Custom Field')
     # x_partner_rank_id = fields.Many2one('res.partner.rank', string='Rank')
 
     x_partner_id = fields.Many2one('res.partner', string='Customer')
