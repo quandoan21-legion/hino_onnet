9fec477cb4d226eb82273297e3f401b29b2c20ff
innocent2003 <leminhtuanthpttranhungdaotx@gmail.com>
2025-02-04 14:14:57 +0700

    Hoan thanh task contact customer

--- models/custom_lead.py
+++ models/custom_lead.py
@@ -1,8 +1,6 @@
-
 import re
-
 from odoo import models, fields, api
-
+from odoo.exceptions import ValidationError
 
 class CustomLead(models.Model):
     _inherit = 'crm.lead'
@@ -12,6 +10,15 @@ class CustomLead(models.Model):
 
     x_partner_id = fields.Many2one('res.partner', string='Customer')
     x_partner_name = fields.Char(string='Customer Name', compute='_compute_customer_name', store=True)
+    x_customer_status = fields.Selection(
+        [
+            ('personal', 'Cá nhân'),
+            ('company', 'Công ty'),
+            ('internal_hmv', 'Nội bộ HMV')
+        ],
+        string="Customer Status",
+        default='personal'
+    )
 
     # x_customer_id = fields.Many2one('res.partner', string='Customer')
     # x_customer_real_id = fields.Char(string='Customer ID', compute='_compute_customer_real_id', store=True, readonly=True)
@@ -37,25 +44,28 @@ class CustomLead(models.Model):
     x_dealer_branch_id = fields.Many2one('res.partner', string='Chi nhánh đại lý')
     x_sale_person_id = fields.Many2one('res.users', string='Nhân viên kinh doanh')
     x_approaching_channel_id = fields.Many2one('hr.employee', string='Kênh tiếp cận')
+    x_state_id = fields.Many2one(
+        'res.country.state', string="State/Province"
+    )
 
+    x_website = fields.Char(string="Website")
+    x_contact_address_complete = fields.Char(
+        string="Địa chỉ cụ thể",
+        help="Địa chỉ chi tiết của khách hàng."
+    )
 
     @api.depends('x_partner_id')
     def _compute_customer_name(self):
         for record in self:
             record.x_partner_name = record.x_partner_id.name if record.x_partner_id else ''
 
-    # @api.depends('x_customer_id')
-    # def _compute_customer_real_id(self):
-    #     for record in self:
-    #         record.x_customer_real_id = str(record.x_customer_id.id) if record.x_customer_id else ''
-    #         record.x_customer_name = record.x_customer_id.name if record.x_customer_id else ''
-
-
-    # @api.onchange('partner_id')
-    # def _onchange_partner_id(self):
-    #     if self.partner_id:
-    #         self.x_indentity_number = self.partner_id.identity_number
-    #         self.x_vat = self.partner_id.vat_number
+    @api.constrains('x_customer_status', 'x_indentity_number', 'x_vat')
+    def _check_customer_status_requirements(self):
+        for record in self:
+            if record.x_customer_status == 'personal' and not record.x_indentity_number:
+                raise ValidationError("Với 'Cá nhân', trường 'Số CCCD/CMND' là bắt buộc.")
+            if record.x_customer_status == 'company' and not record.x_vat:
+                raise ValidationError("Với 'Công ty', trường 'Số ĐKKD (Mã số thuế)' là bắt buộc.")
 
 
     @api.constrains('x_indentity_number')
