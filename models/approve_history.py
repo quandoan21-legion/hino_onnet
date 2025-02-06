d3f006cc834442126fbc9afd180daf5e2fb09aaf
innocent2003 <leminhtuanthpttranhungdaotx@gmail.com>
2025-02-06 13:56:46 +0700

    change to english

--- models/approve_history.py
+++ models/approve_history.py
@@ -5,14 +5,14 @@ class ApproveHistory(models.Model):
     _name = 'approve.history'
     _description = 'Approval History'
 
-    employee_id = fields.Many2one('hr.employee', string="Người xác nhận", required=True)
-    department_id = fields.Many2one('hr.department', string="Phòng ban", related="employee_id.department_id", readonly=True)
-    position_id = fields.Many2one('hr.job', string="Chức vụ", related="employee_id.job_id", readonly=True)
-    status_from = fields.Char(string="Từ trạng thái", required=True)
-    status_to = fields.Char(string="Đến trạng thái", required=True)
-    approve_date = fields.Date(string="Ngày xác nhận", required=True)
-    note = fields.Text(string="Lý do")
-    rank_upgrade_id = fields.Many2one('customer.rank.upgrade', string="Nâng hạng khách hàng")
+    employee_id = fields.Many2one('hr.employee', string="Approver", required=True)
+    department_id = fields.Many2one('hr.department', string="Department", related="employee_id.department_id", readonly=True)
+    position_id = fields.Many2one('hr.job', string="Position", related="employee_id.job_id", readonly=True)
+    status_from = fields.Char(string="From Status", required=True)
+    status_to = fields.Char(string="To Status", required=True)
+    approve_date = fields.Date(string="Approval Date", required=True)
+    note = fields.Text(string="Reason")
+    rank_upgrade_id = fields.Many2one('customer.rank.upgrade', string="Customer Rank Upgrade")
     customer_rank_upgrade_id = fields.Many2one(
-    'customer.rank.upgrade', string="Nâng hạng khách hàng", ondelete="cascade"
-)
+        'customer.rank.upgrade', string="Customer Rank Upgrade", ondelete="cascade"
+    )
