e43be67605e2c29912172da13b560da8d7718272
huang2k3 <hoang2982003@gmail.com>
2025-02-06 11:33:23 +0700

    Sửa tab Sale area detail

---
+++ models/sale_area.py
@@ -0,0 +1,45 @@
+from odoo import models, fields, api, exceptions 
+
+class SaleArea(models.Model):
+    _name = 'sale.area'
+    
+    x_field_sale_name = fields.Char(string='Sale Name', required=True)
+    x_field_sale_code = fields.Char(string='Area Code', required=True, readonly=True, copy=False, default=lambda self: self._generate_sale_code())
+    x_is_free_sales_area = fields.Boolean(string='Free Sales Area', required=False)
+    x_release_time = fields.Datetime(string='Release Time', required=True)
+    x_attach_file = fields.Binary(string='Attach File', required=True)
+    
+    x_sales_area_detali_ids = fields.One2many('sales.area.detail', 'x_sale_area_id', string='Sales Area Detail')
+
+    @api.model
+    def create(self, vals):
+        if vals.get('x_is_free_sales_area') and self.search_count([('x_is_free_sales_area', '=', True)]) > 0:
+            raise exceptions.ValidationError("Đã tồn tại một khu vực bán hàng miễn phí. Vui lòng cập nhật bản ghi hiện tại.")
+        vals.setdefault('x_field_sale_code', self._generate_sale_code())
+        return super().create(vals)
+
+    def write(self, vals):
+        if vals.get('x_is_free_sales_area') and self.search_count([('x_is_free_sales_area', '=', True)]) > 0:
+            raise exceptions.ValidationError("Đã tồn tại một khu vực bán hàng miễn phí. Vui lòng cập nhật bản ghi hiện tại.")
+        return super().write(vals)
+    
+    def _generate_sale_code(self):
+        last_sale_area = self.search([], order='x_field_sale_code desc', limit=1)
+        if last_sale_area:
+            last_number = int(last_sale_area.x_field_sale_code[2:])
+            new_number = last_number + 1
+        else:
+            new_number = 1
+        
+        return f'KV{str(new_number).zfill(3)}'
+    
+    @api.constrains('x_is_free_sales_area')
+    def _check_free_sales_area(self):
+        if self.x_is_free_sales_area and self.search_count([('x_is_free_sales_area', '=', True), ('id', '!=', self.id)]) > 0:
+            raise exceptions.ValidationError(f"{self.x_field_sale_code} - Đã tồn tại một khu vực bán hàng miễn phí. Vui lòng cập nhật bản ghi hiện tại.")
+
+    @api.constrains('x_attach_file_name')
+    def _check_file_format(self):
+        if self.x_attach_file_name and not self.x_attach_file_name.lower().endswith('.pdf'):
+            raise exceptions.ValidationError("Chỉ chấp nhận tệp PDF.")
+    
---
+++ models/sale_area_detail.py
@@ -0,0 +1,41 @@
+from odoo import models, fields, api
+
+class SaleAreaDetail(models.Model):
+    _name = 'sales.area.detail'
+    
+    x_sale_area_id = fields.Many2one('sale.area', string='Sales Area')
+    x_number = fields.Integer(string='STT', readonly=False)   
+    x_code = fields.Many2one('res.country.state', string='Code Province', required=False)
+    x_name = fields.Many2one('res.country.state',string="Name Province", readonly=False)
+
+    @api.model
+    def create(self, vals):
+        sale_area = vals.get('x_sale_area_id')
+        if sale_area:
+            last_number = self.search([('x_sale_area_id', '=', sale_area)], order='x_number desc', limit=1)
+            vals['x_number'] = last_number.x_number + 1 if last_number else 1
+
+        if vals.get('x_code'):
+            vals['x_name'] = vals['x_code']
+        
+        return super(SaleAreaDetail, self).create(vals)
+
+    @api.onchange('x_code')
+    def _onchange_code(self):
+        for record in self:
+            record.x_name = record.x_code if record.x_code else False
+    
+    @api.onchange('x_sale_area_id')
+    def _onchange_sale_area_id(self):
+        if self.x_sale_area_id:
+            last_number = self.search([('x_sale_area_id', '=', self.x_sale_area_id.id)], order='x_number desc', limit=1)
+            self.x_number = last_number.x_number + 1 if last_number else 1
+
+    @api.model
+    def default_get(self, fields_list):
+        res = super(SaleAreaDetail, self).default_get(fields_list)
+        sale_area = res.get('x_sale_area_id')
+        if sale_area:
+            last_number = self.search([('x_sale_area_id', '=', sale_area)], order='x_number desc', limit=1)
+            res['x_number'] = last_number.x_number + 1 if last_number else 1
+        return res
