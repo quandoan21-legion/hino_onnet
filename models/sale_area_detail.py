790efc1ce8134adaec3ae19c4bb84b750e810e6f
huang2k3 <hoang2982003@gmail.com>
2025-02-06 16:27:33 +0700

    UPDATE/TAB_SALE_AREA

--- models/sale_area_detail.py
+++ models/sale_area_detail.py
@@ -4,15 +4,19 @@ class SaleAreaDetail(models.Model):
     _name = 'sales.area.detail.line'
     
     x_sale_area_id = fields.Many2one('sale.area', string='Sales Area')
-    x_number = fields.Integer(string='STT', readonly=False)   
-    x_code = fields.Many2one('res.country.state', string='Code Province', required=False)
-    x_name = fields.Many2one('res.country.state',string="Name Province", readonly=False)
+    x_sale_area_detail = fields.Char(related='x_sale_area_id.x_field_sale_name', string='Sales Area Name', readonly=True)
+    x_number = fields.Integer(string='STT', readonly=True, copy=False)   
+    x_code = fields.Many2one('res.country.state', string='Code Province', required=False
+                             , domain="[('country_id.code', '=', 'VN')]"
+                             )
+    x_name = fields.Many2one('res.country.state',string="Name Province", readonly=True)
 
     @api.model
     def create(self, vals):
-        sale_area = vals.get('x_sale_area_id')
-        if sale_area:
-            last_number = self.search([('x_sale_area_id', '=', sale_area)], order='x_number desc', limit=1)
+        if vals.get('x_sale_area_id'):
+            last_number = self.search(
+                [('x_sale_area_id', '=', vals['x_sale_area_id'])], order='x_number desc', limit=1
+            )
             vals['x_number'] = last_number.x_number + 1 if last_number else 1
 
         if vals.get('x_code'):
@@ -28,18 +32,21 @@ class SaleAreaDetail(models.Model):
     @api.onchange('x_sale_area_id')
     def _onchange_sale_area_id(self):
         if self.x_sale_area_id:
-            last_number = self.search([('x_sale_area_id', '=', self.x_sale_area_id.id)], order='x_number desc', limit=1)
+            last_number = self.search(
+                [('x_sale_area_id', '=', self.x_sale_area_id.id)], order='x_number desc', limit=1
+            )
             self.x_number = last_number.x_number + 1 if last_number else 1
 
     @api.model
     def default_get(self, fields_list):
         res = super(SaleAreaDetail, self).default_get(fields_list)
-        sale_area = res.get('x_sale_area_id')
-        if sale_area:
-            last_number = self.search([('x_sale_area_id', '=', sale_area)], order='x_number desc', limit=1)
+        if res.get('x_sale_area_id'):
+            last_number = self.search(
+                [('x_sale_area_id', '=', res['x_sale_area_id'])], order='x_number desc', limit=1
+            )
             res['x_number'] = last_number.x_number + 1 if last_number else 1
         return res
-    
+
     @api.constrains('x_sale_area_id', 'x_number')
     def _check_unique_number(self):
         for record in self:
