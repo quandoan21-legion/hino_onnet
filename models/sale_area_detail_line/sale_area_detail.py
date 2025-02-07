1a84a0ef0f2499b30a7c4db8cef89322e3f6c5c4
huang2k3 <hoang2982003@gmail.com>
2025-02-07 09:29:22 +0700

    Sửa lại detail và sửa lọc stae

--- models/sale_area_detail_line/sale_area_detail.py
+++ models/sale_area_detail_line/sale_area_detail.py
@@ -7,7 +7,5 @@ class SaleAreaDetail(models.Model):
     x_sale_area_id = fields.Many2one('sale.area', string='Sales Area')
     x_sale_area_detail = fields.Char(related='x_sale_area_id.x_field_sale_name', string='Sales Area Name', readonly=True)
     x_number = fields.Integer(string='STT', readonly=True, copy=False)   
-    x_code = fields.Many2one('res.country.state', string='Code Province', required=False
-                             , domain="[('country_id.code', '=', 'VN')]"
-                             )
+    x_code = fields.Many2one('res.country.state', string='Code Province', required=False)
     x_name = fields.Many2one('res.country.state',string="Name Province", readonly=True)
