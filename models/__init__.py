5f2945a62d4f114aa799aa82cd55b26e511e3dc7
huang2k3 <hoang2982003@gmail.com>
2025-02-10 11:12:06 +0700

    UP FEATURE/CROSS_SALE_REGION

--- models/__init__.py
+++ models/__init__.py
@@ -4,6 +4,7 @@ from .crm_lead_vehicle_interest_line  import tab_custom_lead, tab_custom_lead_me
 from .sale_area import  sale_area, sale_area_methods
 from .sale_area_detail_line import sale_area_detail, sale_area_detail_methods
 from .cross_region_suggest import cross_region_suggest, cross_region_suggest_methods
+from .cross_region_sale import sale_region, sale_region_methods
 from . import customer_rank
 from .partner import custom_partner, custom_bank_line, custom_contact_line
 from . import crm_lead_contact_person
