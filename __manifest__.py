2f0c7b735a6dd65eee450906cf770bb8320aa5fd
Hantuna8459 <55192795+Hantuna8459@users.noreply.github.com>
2025-02-12 10:25:22 +0700

    update init and manifest file

--- __manifest__.py
+++ __manifest__.py
@@ -25,19 +25,36 @@
     'data': [
         'security/ir.model.access.csv',
         'data/lead_sequence.xml',
+        'data/partner_sequence.xml',
+        'data/body_type_sequence.xml',
+        'data/lead_sequence.xml',
         'data/contract_code_sequence.xml'
+        'views/form_views/customer_contact_form_views.xml',
         'views/form_views/custom_lead_views.xml',
         'views/form_views/customer_rank_view.xml',
         'views/form_views/custom_lead_views.xml',
+        'views/tree_views/customer_contact_tree_views.xml',
         'views/form_views/sale_area_view.xml',
-        'views/form_views/contract_view.xml'
+        'views/form_views/cross_region_suggest_view.xml',
+        'views/form_views/cross_region_sale_view.xml',
+        'views/form_views/custom_rank_upgrade_view.xml',
+        'views/form_views/body_type_form_views.xml',
+        'views/form_views/customer_interest_vehicle.xml',
+        'views/tree_views/cross_region_sale_views.xml',
+        'views/tree_views/cross_region_suggest_views.xml',
         'views/tree_views/sale_area_views.xml',
         'views/tree_views/custom_lead_view.xml',
         'views/tree_views/hino_customer_rank.xml',
+        'views/tree_views/hino_customer_upgrade_rank.xml',
         'views/tree_views/approach_channel_view.xml',
+        'views/tree_views/body_type.xml',
+        'views/inherit_views/product_template_views.xml',
+        'views/menu/cross_region_suggest_menu_items.xml',
+        'views/menu/cross_region_sale_menu_items.xml',
         'views/menu/sale_area_menu_items.xml',
         'views/menu/customer_rank_menu_items.xml',
         'views/menu/main_menu_items.xml',
+
     ],
     # only loaded in demonstration mode
     'demo': [
--- models/__init__.py
+++ models/__init__.py
@@ -3,9 +3,15 @@ from .lead import custom_lead, custom_lead_methods
 from .crm_lead_vehicle_interest_line  import tab_custom_lead, tab_custom_lead_methods
 from .sale_area import  sale_area, sale_area_methods
 from .sale_area_detail_line import sale_area_detail, sale_area_detail_methods
-from . import customer_rank
-from . import custom_partner
+from .cross_region_suggest import cross_region_suggest, cross_region_suggest_methods
+from .body_type import body_type, body_type_methods
+from .cross_region_sale import sale_region, sale_region_methods
+from .customer_rank import customer_rank
+from .partner import custom_partner, custom_bank_line, custom_contact_line
 from . import crm_lead_contact_person
 from . import employee
 from . import member_line, owned_team_car_line, approach_channel, customer_follow_up
+from . import request_upgrade_customer_rank
+from . import approve_history
+from .product import custom_product
 from .contract import contract
