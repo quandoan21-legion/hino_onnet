d8f23a2269e0737d4786c4f50b3b7028611375ab
Vy <aum.vypt@gmail.com>
2025-02-20 15:51:15 +0700

    add tree view

--- models/lead/crm_lead_bid_button.py
+++ models/lead/crm_lead_bid_button.py
@@ -18,6 +18,8 @@ class CustomLead(models.Model):
                 'default_investor_address': self.x_contact_address_complete,
                 'default_project_name': self.x_project,
                 'default_area': self.x_area,
+                'default_bid_package_name': self.x_bidding_package,
+                'default_bid_opening_time': self.x_estimated_time_of_bid_opening,
             }
         }
         
