0c8c65d4e3e133aee04353f15de316659802b7f5
quandoan21-legion <qdoan21.legion@gmail.com>
2025-02-28 17:08:08 +0700

    add fixed

--- models/lead/custom_lead.py
+++ models/lead/custom_lead.py
@@ -57,8 +57,8 @@ class CustomLead(models.Model):
     )
     x_customer_follow_up_ids = fields.One2many(
         'crm.follow.up', 'lead_id', string='Customer Follow-Up')
-    x_bidding_package = fields.Char(string='Bidding Package', tracking=True)
     x_project = fields.Char(string='Project', tracking=True)
+    x_bidding_package = fields.Char(string='Bidding Package', tracking=True)
     x_estimated_time_of_bid_opening = fields.Date(
         string='Estimated time of bid opening', tracking=True)
     x_area = fields.Char(string='Area', tracking=True)
